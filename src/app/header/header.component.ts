b94b3f92f8f93c63b7570e615e3d68aa02e77ab3
Jennifer Jacobson <jenniferjacobson@jennifers-air.lan>
2020-09-14 22:13:44 -0500

    studio2 done

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -9,7 +9,9 @@ export class HeaderComponent implements OnInit {
 
   missionName: string = "Mars 2030";
   rocketName: string = "Plasma Max";
+  editingName: boolean = false;
   editingMission: boolean = false;
+
   
   constructor() { }
 
@@ -19,6 +21,11 @@ export class HeaderComponent implements OnInit {
   updateMission(updatedName: string) {
     this.missionName = updatedName;
     this.editingMission = false;
+  }
+
+  updateRocket(updatedName: string) {
+    this.rocketName = updatedName;
+    this.editingRocket = false;
   }
 
 }
